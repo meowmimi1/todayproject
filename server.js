60e84c8fe25acf17e949156b787f1440f7228f95
Mimi Hoang <miminhoang1@gmail.com>
2019-10-15 12:36:15 -0400

    mvp

--- server.js
+++ server.js
@@ -41,5 +41,15 @@ server.use('/api/users', UserRouter);
 server.get('/', (req, res) => {
   res.json({ api: 'up' });
 });
-
+server.get('/api/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.send('error logging out');
+        } else {
+          res.send('good bye');
+        }
+      });
+    }
+  });
 module.exports = server;
